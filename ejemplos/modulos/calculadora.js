cf6a355a2bbaf0809cee4e73ac50168a8d47e102
Angel Sainero Villar <angelsainero@gmail.com>
2023-02-10 22:22:18 +0100

    empezamos con modulos

---
+++ ejemplos/modulos/calculadora.js
@@ -0,0 +1,10 @@
+//fichero calculadora.js
+'use strict'
+
+console.log('inicializo la calculadora')
+
+module.exports = {
+    suma: (a,b) => a + b,
+    // suma: function(a,b) { return a + b}
+    resta: (a,b) => a - b
+}
---
+++ ejemplos/modulos/index.js
@@ -0,0 +1,8 @@
+//fichero index.js
+
+// https://plataforma.keepcoding.io/courses/full-stack-keepcoding-bootcamp-web14/lectures/45421432  (02:54:13)
+'use strict'
+
+const calculadora = require('./calculadora');
+
+console.log(calculadora.suma(2,3))
