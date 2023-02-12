57df405d81902b5bf92745a4d6ad0dbf896ef878
Angel Sainero Villar <angelsainero@gmail.com>
2023-02-12 18:03:18 +0100

    creamos app express

---
+++ ejemplos/ejemplo-express/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// carga la librería de express
+const express = require('express');
+
+// crear la aplicación 
+const app = express()
+
+// añadimos una ruta
+app.get('/',(request, response, next) => {
+    response.send('hola')
+});
+
+// arrancar el servidor
+app.listen(8080, () =>{
+
+    console.log('Servidor HTTP arrancado en http://127.0.0.1:8080');
+});
---
+++ nodeapp/readme.md
@@ -0,0 +1,13 @@
+# NodeApp
+
+Install dependencies with:
+```sh
+npm install
+```
+
+
+
+Strart in development mode:
+```sh
+npm run dev
+```
