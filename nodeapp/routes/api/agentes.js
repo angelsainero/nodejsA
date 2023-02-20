3ab5ea179455dffbca363b3a6b0e9e66ce97ef1a
Angel Sainero Villar <angelsainero@gmail.com>
2023-02-20 13:44:01 +0100

    api metodo put

--- nodeapp/routes/api/agentes.js
+++ nodeapp/routes/api/agentes.js
@@ -12,7 +12,7 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
-
+//https://plataforma.keepcoding.io/courses/full-stack-keepcoding-bootcamp-web14/lectures/45581462  00:38:00
 //metodo que devuelva 1 agente buscando por _id
 // GET /api/agentes/(_id)
 router.get("/:id", async (req, res, next) => {
@@ -20,6 +20,22 @@ router.get("/:id", async (req, res, next) => {
     const id = req.params.id;
     const agente = await Agente.findById(id);
     res.json({ results: agente });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/agentes/:(id) (body)
+// Actualizar un agente
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const agenteActualizado = await Agente.findByIdAndUpdate(id, data, {
+      new: true,
+    }); //los metodos que actualizan devuelven el doc antes, de ahí new:true
+    res.json({ result: agenteActualizado });
   } catch (error) {
     next(error);
   }
